a5b768a599e5d697672670c5f1a61b2a2a143888
Filippo <filippocucina5@gmail.com>
2023-07-24 18:46:29 -0400

    Chapter 1.3 of the Golang Book. Duplicate Lines

---
+++ dup_programs/dup1.go
@@ -0,0 +1,17 @@
+//Dup1 prints the text of each line that appears more than
+//once in the standard input, preceed by its count
+
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	counts := make(map[String]int)
+	input := bufio.NewScanner(os.Stdin)
+
+
+}
